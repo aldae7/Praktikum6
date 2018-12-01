f078134c5a86731c997d308472888505287c4902
aldae7 <aldinofajrian2@mhs.pelitabangsa.ac.id>
2018-12-01 21:36:27 +0700

    latihan2

---
+++ latihan2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+int kali(int a,int b){
+    int r;
+    r = a * b;
+    return (r);
+}
+
+int main(){
+    int y, z;
+    cout << "a + a + a + a +.."; cin >> y;
+    z = kali(5,8);
+    cout << "hasilnya :" << z << endl;
+    cout << endl;
+
+
+}
